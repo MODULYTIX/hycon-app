90b80f70d58472ff8f65a1d0ed0f9a047c5c8781
esaumorales <esaumorales2322@gmail.com>
2025-09-13 22:09:25 -0500

    creando la pagina Hycon

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className='m-0 w-full'>
+            <h1>Header</h1>
+        </div>
+    )
+}
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="hero"></section>
+        <section id=""></section>
+      </main>
+      <Footer />
+    </>
+  );
+}
