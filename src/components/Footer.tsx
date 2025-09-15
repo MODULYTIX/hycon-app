c229692160cee7b62fd9f65eab36b4dfc590e28c
esaumorales <esaumorales2322@gmail.com>
2025-09-15 08:35:07 -0500

    fixed(component). fixed component footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-      <footer className="h-[60px] w-full bg-primary text-white flex items-center justify-center">
-        <h1>Footer</h1>
+      <footer className="h-[60px] w-full bg-primary text-white flex items-center justify-center m-auto z-50">
+        <h1 className='text-3xl'>Footer</h1>    
       </footer>
     );
   }
